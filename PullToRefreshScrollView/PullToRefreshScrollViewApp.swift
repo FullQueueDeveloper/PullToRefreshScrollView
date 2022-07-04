cca9743f94d0585db701d1d99ca2e36dce872358
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2022-07-04 14:19:54 -0400

    Initial Commit

---
+++ PullToRefreshScrollView/PullToRefreshScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefreshScrollViewApp.swift
+//  PullToRefreshScrollView
+//
+//  Created by Full Queue Developer on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PullToRefreshScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
