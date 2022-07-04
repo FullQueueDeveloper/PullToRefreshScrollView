2643422a1dc465f912c7dd2bea27ff9b3b76b52a
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2022-07-04 18:27:44 -0400

    Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(
+    name: "PullToRefreshScrollView",
+    platforms: [
+        .iOS(.v14),
+    ],
+    products: [
+        .library(
+            name: "PullToRefreshScrollView",
+            targets: ["PullToRefreshScrollView"]),
+    ],
+    dependencies: [],
+    targets: [
+      .target(name: "PullToRefreshScrollView",
+        dependencies: [],
+        path: "PullToRefreshScrollView")
+    ])
